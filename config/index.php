5685f981e09527e844f9cbb4fd3229a8703160bd
Erick Penado <penado188@gmail.com>
2023-02-25 23:15:41 -0600

    Agregando config para definir variables de root y links

---
+++ config/index.php
@@ -0,0 +1,13 @@
+<?php
+
+//Definir rutas constantes
+define('root', $_SERVER['DOCUMENT_ROOT'] . '/');
+define('view', root.'/c022021/');
+define('views', root.'/c022021/views/');
+
+//definir rutas de links
+define('uriLogin', 'views/login/');
+define('uriRegistrar', 'views/registrar/');
+define('uriInicio', '/c022021/');
+
+?>
